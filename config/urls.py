d73cc5bb87f5b20cc55b9b0c3f70b3421df4aee3
rakibulislam8226 <rakibulislam8226@gmail.com>
2025-05-27 15:58:48 +0600

    rest folder added insides apps for related backed endpoints

--- config/urls.py
+++ config/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # apps
+    path("api/v1/", include("apps.articlesio.rest.urls")),
+    path("api/v1/", include("apps.authio.rest.urls")),
 ]
